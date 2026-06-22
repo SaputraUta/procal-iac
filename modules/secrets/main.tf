4744d4a3492ad2d1a2993c8f87e59c22270e43b2
Saputra <saputra.uta50@gmail.com>
2026-06-22 13:28:10 +0700

    style: tofu fmt

--- modules/secrets/main.tf
+++ modules/secrets/main.tf
@@ -1,7 +1,7 @@
 resource "aws_secretsmanager_secret" "this" {
-  for_each    = var.secrets
-  name        = "${var.name_prefix}/${each.key}"
-  description = each.value.description
-  tags        = var.tags
+  for_each                = var.secrets
+  name                    = "${var.name_prefix}/${each.key}"
+  description             = each.value.description
+  tags                    = var.tags
   recovery_window_in_days = 0
 }
